eb94fccdd837724d1c82780fc11375ff69795115
TalissonMelo <talissonmelorodrigues@gmail.com>
2023-01-01 12:02:16 -0300

    Iniciando projeto para verificar e utilizar atualizações de SPRING BOOT 3.0 e JAVA versão 17

---
+++ src/main/java/com/talissonmelo/AnimeApplication.java
@@ -0,0 +1,13 @@
+package com.talissonmelo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/talissonmelo/AnimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talissonmelo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
